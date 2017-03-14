467792a1a85fb10381d037d50f926c8bccb66408
Thomas Skarshaug <thomskar@stud.ntnu.no>
2017-03-14 15:49:23 +0100

    Made new branch query for query development

--- src/main/LoadDriver.java
+++ src/main/LoadDriver.java
@@ -6,6 +6,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
+import java.util.ArrayList;
 
 // Notice, do not import com.mysql.jdbc.*
 // or you will have problems!
@@ -62,9 +63,9 @@ public class LoadDriver {
 	private static ResultSet rs = null;
 	private static ResultSetMetaData rsmd = null;
 
+	private static ArrayList<String> kolonner;
 
-
-	public static void sporring(String query){
+	public static ArrayList<String> sporring(String query){
 		try{
 
 			stmt = conn.createStatement();
@@ -82,66 +83,51 @@ public class LoadDriver {
 			}
 
 			/*
-
-				VELGER IKKE ALLE KOLONNENE,
-
-			 */
-
-		/*	int count = 0;
-
-			for (int i = 1; i <= rsmd.getColumnCount(); ++i){
-				while (rs.next()) {
-					kolonne1 = rs.getString(i);
-					//kolonne2 = rs.getString(2);
-					//System.out.println(kolonne1 + " - " + kolonne2);
-					output.append(kolonne1  + "\n");
-
-					System.out.println("i = " + i);
-					count++;
-					System.out.println("Count = " + count);
-				}
-
-			}
-
-		*/
-
-		/*	int countTest = 0;
-			while (rs.next() && countTest < 2) {
-				kolonne1 = rs.getString(1);
-				System.out.println("Kolonne1");
-				kolonne2 = rs.getString(2);
-				System.out.println("Kolonne2");
-				kolonne3 = rs.getString(3);
-				System.out.println("Kolonne3");
-				kolonne4 = rs.getString(4);
-				System.out.println("Kolonne4");
-				output.append(kolonne1 + " - " + kolonne2 + " - " + kolonne3 + " - " + kolonne4 + "\n");
-
-				countTest++;
-
-				System.out.println(countTest);
-			}
-		*/
-			String kolonner [] = new String[rsmd.getColumnCount()];
-
 			int count = 0;
+			String kols [] = new String[rsmd.getColumnCount()];
 
 			while(rs.next()){
 				for (String s : kolonner) {
-					kolonner[count] = rs.getString(count + 1);
+					kols[count] = rs.getString(count + 1);
 					if (count + 1 < rsmd.getColumnCount()) {
-						output.append(kolonner[count] + " - ");
+						output.append(kols[count] + " - ");
 					} else {
-						output.append(kolonner[count] + "\n");
+						output.append(kols[count] + "\n");
 					}
 					count++;
 				}
 				count = 0;
 			}
 
+			*/
+
+			kolonner = new ArrayList<String>();
+
+			while(rs.next()){
+				for (int i = 0; i < rsmd.getColumnCount(); i++) {
+					kolonner.add(rs.getString(i + 1));
+				/*	if (i + 1 < rsmd.getColumnCount()) {
+						output.append(kolonner.get(i) + " - ");
+					} else {
+						output.append(kolonner.get(i) + "\n");
+					}
+				*/
+				}
+			}
 
 		}catch (SQLException e){
 			System.out.println("SQLException: " + e.getMessage());
+		}
+		printKolonner(kolonner);
+		return kolonner;
+	}
+
+	public static void printKolonner(ArrayList<String> kol){
+		for (int i = 0; i < kol.size(); i++){
+
+			if (rsmd.)
+
+			System.out.println(i);
 		}
 	}
 
@@ -154,7 +140,23 @@ public class LoadDriver {
 		}
 	}
 
+	public void formatQuery(String command){
 
+		if (command.equals("HENT NOTAT")){
+			sporring("SELECT notat FROM OVELSE");
+		}
+
+
+	}
+
+	public  void getReport(String command){
+
+		ArrayList<String> vekt = new ArrayList<String>();
+
+		if (command.equals("HENT RAPPORT")){
+			vekt = sporring("SELECT MAX(vekt) FROM STYRKE");
+		}
+	}
 	public static void createFrame()
 	{
 		frame = new JFrame("Database GUI");
@@ -197,27 +199,12 @@ public class LoadDriver {
 
 		public void actionPerformed(final ActionEvent ev)
 		{
-		/*	if (!input.getText().trim().equals(""))
-			{
-				String cmd = ev.getActionCommand();
-				if (ENTER.equals(cmd))
-				{
-					output.append(input.getText());
-					if (input.getText().trim().equals(testString)) output.append(" = " + testString);
-					else output.append(" != " + testString);
-					output.append("\n");
-				}
-			}
-			input.setText("");
-			input.requestFocus();
-
-		*/
 
 			if (!input.getText().trim().equals("")){
 				String cmd = ev.getActionCommand();
 				if (ENTER.equals(cmd)){
 					sporring(input.getText());
-					insert(input.getText());		// FINN PÅ EN BEDRE MÅTE!! FÅR EXCEPTION //
+		//			insert(input.getText());		// FINN PÅ EN BEDRE MÅTE!! FÅR EXCEPTION //
 					output.append("\n");
 				}
 			}
